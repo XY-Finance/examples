bc07feaf8e624ad4a30b0bb186f4c935fe631c69
a1740942002 <a1740942002@me.com>
2023-12-12 18:22:00 +0800

    refactor: Remove deprecated and unused files and dependencies
    - Deleted unnecessary Gitignore files in the `widget-iframe/v2/nextjs` and `widget-iframe/v2/vite` folders
    - Removed unused files, imports, exports, and code snippets in various files
    - Updated dependencies and dev dependencies in `widget-iframe/v1/nextjs/package.json`
    - Refactored code in `widget-iframe/v1/nextjs/app/page.tsx` to use the `queryString` library for constructing URL query parameters

--- widget-iframe/v1/nextjs/app/page.tsx
+++ widget-iframe/v1/nextjs/app/page.tsx
@@ -1,9 +1,30 @@
+import queryString from 'query-string'
+
 export default function Home() {
-  const src = 'https://widget.xy.finance'
+  const config = {
+    fromTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    sourceChainId: '1',
+    toTokenAddress: '0x77777777772cf0455fB38eE0e75f38034dFa50DE',
+    targetChainId: '1',
+    amount: 1,
+    slippage: '1',
+    orientation: 'portrait',
+    theme: 'dark',
+    lockmode: 'none',
+    pagecolor: null,
+    referrer: null
+  }
+  const url = queryString.stringify(config)
+  // amount=1&fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&lockmode=none&orientation=portrait&pagecolor&referrer&slippage=1&sourceChainId=1&targetChainId=1&theme=dark&toTokenAddress=0x77777777772cf0455fB38eE0e75f38034dFa50DE
+
+  const src = `https://widget.xy.finance/?${url}`
+  // "You can change the iframe size. Recommended settings: width 480, height 700."
+  const width = 480
+  const height = 700
 
   return (
     <div>
-      <iframe width={480} height={700} src={src} />
+      <iframe width={width} height={height} src={src} />
     </div>
   )
 }
--- widget-iframe/v2/nextjs/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  const src = 'https://widget.xy.finance'
-
-  return (
-    <div>
-      <iframe width={480} height={700} src={src} />
-    </div>
-  )
-}

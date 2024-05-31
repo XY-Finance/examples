d12087b7b71687d2bf0878f7bfbfb282bb96cb27
a1740942002 <a1740942002@me.com>
2024-05-31 12:10:45 +0800

    refactor: Refactor and rename files for Vite Vue widget iframe.
    - Renamed multiple files and directories in the `widget-iframe/v1/vite` folder to `widget-iframe/v1/vite-react` for consistency
    - Added new files for Vue and React setups in the `widget-iframe/v1/vite-vue` directory
    - Updated gitignore files to include necessary ignores for the project

---
+++ widget-iframe/v1/vite-vue/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import queryString from 'query-string'
+
+const config = {
+  // The address '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' stands for the native token.
+  fromTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+  sourceChainId: '1',
+  toTokenAddress: '0x77777777772cf0455fB38eE0e75f38034dFa50DE',
+  targetChainId: '1',
+  amount: 1,
+  slippage: '1',
+  theme: 'dark',
+  lockmode: 'none',
+  referrer: '0x856ebdc948afFCc6E24FCe08458051B19de652F6',
+  commissionRate: 2000
+}
+
+const baseUrl = 'https://widget.xy.finance?'
+const searchParams = queryString.stringify(config)
+const iframeSrc = ref(baseUrl + searchParams)
+</script>
+
+<template>
+  <div class="container">
+    <iframe :width="480" :height="700" :src="iframeSrc" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
